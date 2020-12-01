2af1a536f73170d7d63dbfe11a922a9184519803
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2020-11-30 20:58:42 -0500

    Working with collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = config => {
+
+    // Returns work items, sorted by display order
+    config.addCollection('posts', collection => {
+        return collection
+            .getFilteredByGlob('./src/posts/*.md');
+    });
+
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/')
     config.addPassthroughCopy('./src/admin/')
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Die Kaffeezeit
 
 Projekt für Intensivkursdeutsch II
+
+Autoren:
+
+- Juan Bernardo Benavies
+- Ana Bernal
+- Valentina Giraldo
+- Carlos Acuña
+- José Miguél Pérez
+
+Web-entwicklung:
+
+- Juan Bernardo Benavides
