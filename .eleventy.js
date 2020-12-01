261eb031a799a63a32e1c660912e37cc058fa149
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2020-11-30 23:09:22 -0500

    Feat: Get categories from collections

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,32 @@ module.exports = config => {
             .getFilteredByGlob('./src/posts/*.md');
     });
 
+    config.addCollection("categories", collection => {
+        let categorySet = new Set();
+        collection.getAll().forEach( item => {
+            if ( "tags" in item.data ) {
+                let tags = item.data.tags;
+
+                tags = tags.filter( item =>{
+                    switch (item ) {
+                        case "all":
+                        case "nav":
+                        case "post":
+                        case "posts":
+                            return false;
+                    }
+                    return true;
+                });
+
+                for (const tag of tags) {
+                    categorySet.add(tag);
+                }
+            }
+        });
+
+        return [...categorySet]
+    })
+
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/')
     config.addPassthroughCopy('./src/admin/')
