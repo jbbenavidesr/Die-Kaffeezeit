ca3fd979518d2d470102d709b77dde2baaed5ce8
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2020-12-03 22:16:50 -0500

    Feat: finish a great deal...

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = config => {
 
+    // Transforms
+    const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+
+    // Create a helpful production flag
+    const isProduction = process.env.NODE_ENV === 'production';
+
     // Returns post items
     config.addCollection('posts', collection => {
         return collection
@@ -52,6 +58,11 @@ module.exports = config => {
 
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/admin/')
+
+    // Only minify HTML if we are in production because it slows builds _right_ down
+    if (isProduction) {
+        config.addTransform('htmlmin', htmlMinTransform);
+    }
 
     // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
     config.setUseGitIgnore(false);
